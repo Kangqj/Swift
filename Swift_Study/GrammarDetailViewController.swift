5a26df80e9b68fb450b6d59d0d22872341cde2bb
Kangqj <tiankong.100@163.com>
2022-02-11 16:31:34 +0800

    swift本地数据读取

--- Swift_Study/GrammarDetailViewController.swift
+++ Swift_Study/GrammarDetailViewController.swift
@@ -6,14 +6,46 @@
 //
 
 import UIKit
+import WebKit
 
 class GrammarDetailViewController: UIViewController {
 
-    @IBOutlet weak var mTextView: UITextView!
+    @IBOutlet weak var mWebView: WKWebView!
+    var urlS : String?
+    var note : String?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        let rightItem = UIBarButtonItem(title: "笔记", style: .plain, target: self, action: #selector(showNote))
+        self.navigationItem.rightBarButtonItem = rightItem;
+        
+        /*
+         可选项无法直接使用，需要展开之后才能使用，确保变量不为nil后才可以使用，这个在编辑阶段就确保了程序的稳定性
+         使用可选项绑定来判断可选项是否包含值，如果包含就把值赋给一个临时常量或变量
+         可选绑定可以与if或while语句使用来检查可选项内部的值，并赋值给一个临时常量或变量
+         */
+        if let urlm = urlS {
+            let urlr = URL(string: urlm)
+            if let url = urlr {
+                let request = URLRequest(url: url)
+                mWebView.load(request)
+            }
+        }
+        
+        /*
+        在可选项后加问号（?）进行函数调用
+        如果可选项不为nil，返回一个可选项结果，否则返回nil，函数返回得到的可选项直接使用也是不行的，也需要判断展开才可以
+         如：
+         urlS?.append("aa")
+         */
+        
+        /*
+         !会强制认为可选类型变量有值，当可选类型变量为nil时，程序就会crash
+         */
+//        let url = URL(string: urlS!)!
+//        let request = URLRequest(url: url)
+//        mWebView.load(request)
     }
     
     static func instantiate() ->GrammarDetailViewController?
@@ -25,6 +57,14 @@ class GrammarDetailViewController: UIViewController {
         return vc
     }
     
+    @objc func showNote() {
+        let alertVC = UIAlertController(title: "笔记", message: note, preferredStyle: .actionSheet)
+        let okAction = UIAlertAction(title: "OK", style: .default) {
+            action in
+        }
+        alertVC.addAction(okAction)
+        self.present(alertVC, animated: true, completion: nil)
+    }
     /*
     // MARK: - Navigation
 
--- Swift_Study/GrammarTableViewController.swift
+++ Swift_Study/GrammarTableViewController.swift
@@ -9,18 +9,41 @@ import UIKit
 
 class GrammarTableViewController: UITableViewController {
 
-    var list = [[String:String]]()
+    var list = [[String:Any]]()
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         // Uncomment the following line to preserve selection between presentations
         // self.clearsSelectionOnViewWillAppear = false
-
+        
         // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
         // self.navigationItem.rightBarButtonItem = self.editButtonItem
         
-        let dict1 = ["title":"可选", "detail":"?，!"]
-        list.append(dict1)
+        if let filepath = Bundle.main.path(forResource: "grammar_list", ofType: "json") {
+            do {
+                let contents = try String(contentsOfFile: filepath)
+                let dic = contents.toDictionary()
+//                let dic = stringValueDic(contents)
+                guard let arr = dic["data_list"] as? [[String:String]] else {
+                    return
+                }
+                list.append(contentsOf: arr)
+            } catch {
+                // 内容无法加载
+            }
+        } else {
+            // 文件未找到
+        }
+        
+//        let dict1 = ["title":"可选项Optional", "detail":"?，!可选项,判断nil，展开，可选链", "url":"https://zhuanlan.zhihu.com/p/462800711"]
+//        let dict2 = ["title":"测试可选项为nil的场景", "detail":"nil"]
+//        let dict3 = ["title":"guard的用法", "detail":"guard语句，if判断", "url":"https://www.jianshu.com/p/67f3e3ee6a32"]
+//        let dict4 = ["title":"as、as!、as? 三种操作符", "detail":"转型，类型判断，消除二义性", "url":"https://www.cnblogs.com/dukework/p/6553714.html"]
+//
+//        list.append(dict1)
+//        list.append(dict2)
+//        list.append(dict3)
+//        list.append(dict4)
     }
 
     // MARK: - Table view data source
@@ -38,8 +61,8 @@ class GrammarTableViewController: UITableViewController {
         
         cell?.accessoryType = .disclosureIndicator
         let dic = list[indexPath.row]
-        cell?.textLabel?.text = dic["title"];
-        cell?.detailTextLabel?.text = dic["detail"];
+        cell?.textLabel?.text = dic["title"] as? String;
+        cell?.detailTextLabel?.text = dic["detail"] as? String;
         
         return cell!;
     }
@@ -49,8 +72,29 @@ class GrammarTableViewController: UITableViewController {
             return
         }
         let dic = list[indexPath.row]
-        vc.title = dic["title"]
+        vc.title = dic["title"] as? String
+        vc.urlS = dic["url"] as? String
+        vc.note = dic["note"] as? String
         self.navigationController?.pushViewController(vc, animated: true)
+    }
+    
+    
+    // MARK: 字符串转字典
+    func stringValueDic(_ str: String) -> [String : Any]?{
+        let data = str.data(using: String.Encoding.utf8)
+        if let dict = try? JSONSerialization.jsonObject(with: data!,
+                        options: .mutableContainers) as? [String : Any] {
+            return dict
+        }
+
+        return nil
+    }
+    
+    // MARK: 字典转字符串
+    func dicValueString(_ dic:[String : Any]) -> String?{
+        let data = try? JSONSerialization.data(withJSONObject: dic, options: [])
+        let str = String(data: data!, encoding: String.Encoding.utf8)
+        return str
     }
     
     /*
